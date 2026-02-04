a67f6624848f5b6e8389be7036f755f2437d0bde
TBAmphi <leandre.decq@gmail.com>
2026-02-04 16:24:23 +0100

    Feat: Functiunal Ads
    Functiunal ads in LeandreScene.
    To do -> A prefab of AdsManager that able to work with all types of ads everywhere.

--- Assets/Script/Ads/AdsManager.cs
+++ Assets/Script/Ads/AdsManager.cs
@@ -1,9 +1,54 @@
+using System;
 using UnityEngine;
 
 public class AdsManager : MonoBehaviour
 {
-    public void StartAd()
+
+    [SerializeField] InterstitialAd interstitialAdManager;
+    [SerializeField] RewardedAds rewardedAdManager;
+    [SerializeField] bool isAdLoading = false;
+    void Start()
     {
-        Debug.Log("Start ad");
+        Debug.Log("AdsManager Started");
+        if(interstitialAdManager == null)
+        {
+            //Debug.Log("No InterstitialAd.cs");
+            interstitialAdManager = gameObject.GetComponent<InterstitialAd>();
+        }
+
+        if(rewardedAdManager == null)
+        {
+            rewardedAdManager = gameObject.GetComponent<RewardedAds>();
+        }
+    }
+
+    public async void ExampleInterstitialAd()
+    {
+        if(!isAdLoading)
+        {
+            isAdLoading =true;
+            Debug.Log("Example Timer Started");
+            await Awaitable.WaitForSecondsAsync(3f);
+            Debug.Log("Example Timer Finished");
+            interstitialAdManager.LoadAd();
+            interstitialAdManager.ShowAd();
+            isAdLoading = false;
+        }
+    }
+
+    public async void ExampleRewarded()
+    {
+        Debug.Log("Here in Example");
+        if(!isAdLoading)
+        {
+            Debug.Log("In IT");
+            isAdLoading =true;
+            Debug.Log("Example Timer Started");
+            await Awaitable.WaitForSecondsAsync(3f);
+            Debug.Log("Example Timer Finished");
+            rewardedAdManager.LoadAd();
+            rewardedAdManager.ShowAd();
+            isAdLoading = false;
+        }
     }
 }
