3b5b2698a540963fc8d703f4700b32e8cb5d1ed8
Ethn8 <tometmoi37@gmail.com>
2026-03-19 17:04:29 +0100

    script
    tous les scripts sont là mais je ne peux pas les mettres à cause d'une erreur de package que je peux pas installé et d'un script

---
+++ Assets/Script/Menue/MainMenu.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//Menu principal
+public class MainMenu : MonoBehaviour
+{
+
+  [Header("Scène")]
+  public string levelToLoad;
+  public string skinScene;
+  public GameObject SettingsWindow;
+
+
+  //Boutton commencer le jeux
+  public void StartGame()
+  {
+    SceneManager.LoadScene(levelToLoad);
+  }
+
+  public void Skin()
+  {
+    SceneManager.LoadScene(skinScene);
+  }
+
+  //Boutton ouvrire les paramètres
+  public void SettingButton()
+  {
+    SettingsWindow.SetActive(true);
+  }
+
+  //Boutton quitter le jeux
+  public void QuitGames()
+  {
+    Application.Quit();
+  }
+
+  //bouton fermer les paramètres
+  public void CloseSettingsWindow()
+  {
+    SettingsWindow.SetActive(false);
+  }
+
+  void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+      SettingsWindow.SetActive(false);
+    }
+  }
+
+  //bouton ouvrire les credits
+  public void LoadCreditsScene()
+  {
+    SceneManager.LoadScene("Credits");
+  }
+
+  //bouton resets playerpref
+  public void ResetPref()
+  {
+    PlayerPrefs.DeleteAll(); //supprime playerpref
+    PlayerPrefs.Save(); // assure enregistrement sur disk
+  }
+}
